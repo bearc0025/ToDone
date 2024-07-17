947f00fa889d2e735f814f45ddca149de62f454a
bearc0025 <bcahill@etsy.com>
2024-07-17 11:11:44 -0500

    Initial Commit

---
+++ ToDone/ToDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoneApp.swift
+//  ToDone
+//
+//  Created by Bear Cahill on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
